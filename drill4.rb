f30f3b8a35c80d7acbef6bd3ffea27c335f2144b
Daiki925864 <daikiokada9097@gmail.com>
2023-08-20 00:04:50 +0900

    Update drill4.rb

--- drill4.rb
+++ drill4.rb
@@ -1,3 +1,25 @@
+# 問題
+
+# class Article
+
+#   def initialize(author, title, content)
+#     @author = author
+#     @title = title
+#     @content = content
+#   end
+
+# end
+
+# 上記のコードに追加を行い、以下の出力結果を得られるようにしてください。ただし、クラスとインスタンスを使用するものとします。
+
+# ターミナル
+
+# 著者: 阿部
+# タイトル: Rubyの素晴らしさについて
+# 本文: Awesome Ruby!
+# -----------------------------------------------------------------------------------------------------------------
+
+
 class Article
 
   def initialize(author, title, content)
