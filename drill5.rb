3e43adba2aae23853cebe756b77520b184de19a4
Daiki925864 <daikiokada9097@gmail.com>
2023-08-21 21:56:19 +0900

    Update drill5.rb

--- drill5.rb
+++ drill5.rb
@@ -1,3 +1,23 @@
+# 問題
+# 以下のプログラムを実行するとエラーが起きます。
+# ①エラーが起きた原因
+# ②正しいソースコード（引数を用いたコードにしましょう）
+# をそれぞれ答えてください。
+
+# price = 300
+
+# def calculate_price_with_tax
+#   tax = 0.1
+#   return price + price * tax
+# end
+
+# calculate_price_with_tax
+# price = 300
+
+# -----------------------------------------
+
+# ① メソッドに引数が設定されておらず、メソッド外の変数はメソッド内で使用できないため
+
 price = 300
 
 def calculate_price_with_tax(price)
