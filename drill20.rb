87705a00e499d87ecf8aff46f7a4112a8474e40b
Daiki925864 <daikiokada9097@gmail.com>
2023-09-12 23:52:18 +0900

    Create drill20.rb

---
+++ drill20.rb
@@ -0,0 +1,12 @@
+# 問題
+# 任意の文字列に"code"が、左から何文字目に出てくるかを返し、その数を出力するメソッドを作りましょう。
+
+# 出力例：
+# count_code("codexxcode") → 1
+# count_code("aaacodebbb") → 4
+# count_code("cozexxcode") → 7
+
+----------------------------------------------------------------------------------------------
+def count_code(str)
+  puts str.index("code", 0) + 1
+end
